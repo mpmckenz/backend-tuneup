49e55e15346646d0696c4a6a7863874aff831ae8
Michael McKenzie <mikemckenzie987@gmail.com>
2019-03-18 18:42:48 -0400

    list comp dupl finder but issue printing mult times

--- tuneup.py
+++ tuneup.py
@@ -2,15 +2,25 @@
 # -*- coding: utf-8 -*-
 """Tuneup assignment"""
 
-__author__ = "???"
+__author__ = "Michael McKenzie"
 
 import cProfile
 import pstats
+import timeit
 
 
 def profile(func):
     """A function that can be used as a decorator to measure performance"""
-    raise NotImplementedError("Complete this decorator function")
+    # cProfile.run(pr_func)
+    def profile_function(*args, **kwargs):
+        pr = cProfile.Profile()
+        pr.enable()
+        pr_func = func(*args, **kwargs)
+        pr.disable()
+        ps = pstats.Stats(pr).sort_stats('cumulative')
+        ps.print_stats()
+        return pr_func
+    return profile_function
 
 
 def read_movies(src):
@@ -28,20 +38,27 @@ def is_duplicate(title, movies):
     return False
 
 
+@profile
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list"""
     movies = read_movies(src)
     duplicates = []
-    while movies:
-        movie = movies.pop()
-        if is_duplicate(movie, movies):
-            duplicates.append(movie)
-    return duplicates
+    return set([movie for movie in movies if movies.count(movie) > 1])
+
+    # mv_d = {}
+    # for movie in movies:
+    #     if movie in mv_d:
+    #         duplicates.append(movie)
+    #     else:
+    #         mv_d[movie] = ''
+    # return duplicates
 
 
 def timeit_helper():
     """Part A:  Obtain some profiling measurements using timeit"""
-    # YOUR CODE GOES HERE
+    t = min(timeit.Timer('find_duplicate_movies("movies.txt")', setup='from __main__ import find_duplicate_movies').repeat(
+        repeat=7, number=3))
+    print "Best time across 7 repeats of 3 runs per repeat: {} sec".format(t)
 
 
 def main():
@@ -52,4 +69,5 @@ def main():
 
 
 if __name__ == '__main__':
+    timeit_helper()
     main()
